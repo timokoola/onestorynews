05c40c5b162d55c9e08abeb9fdbe7bfdd63210da
Timo Koola <timo.koola@gmail.com>
2013-02-03 22:44:42 +0200

    Signature added to the mix

--- twittergw.py
+++ twittergw.py
@@ -39,29 +39,46 @@ class TweepyHelper:
         auth.set_access_token(accesstoken, accesssec)
         self.api = tweepy.API(auth)
 
-if __name__ == "__main__":
+
+def generate_tweet(tid,signature=""):
+    urlre = re.compile("http://\S+")
+    text_part = api.get_status(tid).text
+    url_part = " ".join(urlre.findall(text_part))
+    text_part = urlre.sub("", text_part)
+    from_part = api.get_status(tid).author.screen_name
+    tweet = text_part[:120 - len(url_part)] + " " + url_part + " via @" + from_part + " " + signature
+    return tweet
+
+def candidate_ids(api):
+    return " ".join([str(x.id_str+ " ")*x.retweet_count for x in api.home_timeline(count=100) if ignoreTweet(x)]).split()
+
+def pick_tweet(api):
+    return Counter(candidate_ids(api)).most_common(10)[0][0]
+
+def handle_command_line():
     parser = argparse.ArgumentParser(description="Process news tweets. Send a tweet of latest breaking news. (It's kind of magic, but not really)")
-    parser.add_argument("-t","--test", help="Run a test run and list top 10 candidates", action="store_true")
-    parser.add_argument("-k","--keyfile",help="Twitter account consumer and accesstokens")
-    parser.add_argument("-i","--ignore", help="ignore tweets containing this keywords",default="twitter" )
+    parser.add_argument("-t", "--test", help="Run a test run and list top 10 candidates", action="store_true")
+    parser.add_argument("-k", "--keyfile", help="Twitter account consumer and accesstokens")
+    parser.add_argument("-i", "--ignore", help="ignore tweets containing this keywords", default="twitter")
+    parser.add_argument("-s", "--signature", help="Sign Tweet with a ^name", default="")
     args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = handle_command_line()
 
     api = (TweepyHelper(args.keyfile)).api
 
-    words = " ".join([str(x.id_str+ " ")*x.retweet_count for x in api.home_timeline(count=100) if ignoreTweet(x)]).split()
-    tweetid = Counter(words).most_common(10)[0][0]
-    urlre = re.compile("http://\S+")
-    text_part = api.get_status(tweetid).text
-    url_part = " ".join(urlre.findall(text_part))
-    text_part = urlre.sub("",text_part)
-    from_part = api.get_status(tweetid).author.screen_name
-    tweet = text_part[:120-len(url_part)]+ " " + url_part + " via @" + from_part
+    tweetid = pick_tweet(api)
+
+    tweet = generate_tweet(tweetid,args.signature)
     print tweet, "score %s" % api.get_status(tweetid).retweet_count
     if not args.test:
         api.update_status(tweet)
     else:
-        candidates = Counter(words).most_common(10)
+        candidates = Counter(candidate_ids(api)).most_common(10)
         for c in candidates:
             twid = c[0]
             text = api.get_status(twid).text
-            print "Candidate with score %d: %s" % (c[1],text) 
+            username = api.get_status(twid).author.screen_name
+            print "Candidate from %s with score %d: %s" % (username,c[1],text)
